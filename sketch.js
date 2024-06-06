fba604890a76b8bfdbf4d35d150c74232c0e2577
tilapinhafrita <00001111220682sp@al.educacao.sp.gov.br>
2024-06-06 15:37:28 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function draw() {
+  
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,50);
+ 
+  
+  
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
+  }
+  
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
+  }
+  
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+    
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+  
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+    
+}
